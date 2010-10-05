88c6af988131473a94cdf83259fc0c19b5ecb261
AYUkawa,Yasuyuki <ayu+github@yamayo.to>
2010-10-05 14:13:14 +0900

    Updated scratch-log.
    modified:   app-emacs/scratch-log/Manifest
    modified:   app-emacs/scratch-log/files/99scratch-log-gentoo.el
    renamed:    app-emacs/scratch-log/scratch-log-0.0.1.ebuild -> app-emacs/scratch-log/scratch-log-20101002.ebuild

--- app-emacs/scratch-log/files/99scratch-log-gentoo.el
+++ app-emacs/scratch-log/files/99scratch-log-gentoo.el
@@ -1,4 +1,5 @@
-(require 'scratch-log.el)
+(add-to-list 'load-path "@SITELISP@")
+(require 'scratch-log)
 
 (setq sl-scratch-log-file (expand-file-name "~/.emacs.d/.scratch-log"))
 (setq sl-prev-scratch-string-file (expand-file-name "~/.emacs.d/.scratch-log-prev"))
